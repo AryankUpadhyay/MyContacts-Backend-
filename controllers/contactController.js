69952fb994f0ff1f49d10fcfee4374f734fcb52c
AryankUpadhyay <iamaryankupadhyay@gmail.com>
2023-09-26 04:56:51 +0530

    added aysnc handler and Error Handlers

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,50 +1,62 @@
-//description get all contacts 
+const asyncHandler = require("express-async-handler");
+
+//description get all contacts
 // routes GET/api/contacts
 // access-> public
 // when we introduce authentication we would make it private
-const getContacts = (req, res) =>{
-    res.status(200).json({message: "Get all Contacts"});
-};
 
+const getContacts = asyncHandler(async (req, res) => {
+  // whenever we interact with database we get Promises therefore we add asyc:
+  res.status(200).json({ message: "Get all Contacts" });
+});
+
+// whenever we use Async we should add it try catch.
+// but in express we use express async handler so to avoid try catch .. it works like try catch only
 
 //description Create contact
 // routes POST/api/contacts
 // access-> public
 // when we introduce authentication we would make it private
-const createContact = (req, res) =>{
-    console.log("The request body is :", req.body);
-        res.status(201).json({messges: "Create Contact"});
-};
-
+const createContact = asyncHandler(async (req, res) => {
+  console.log("The request body is :", req.body);
+  // error handling
+  const { name, email } = req.body;
+  if (!name || !email) {
+    res.status(400);
+    throw new Error("All fields are mandatory");
+  }
+  res.status(201).json({ messges: "Create Contact" });
+});
 
 //description get contact
 // routes GET/api/contacts/:id
 // access-> public
 // when we introduce authentication we would make it private
-const getContact = ((req, res)=>{
-    res.status(200).json({messges: `update Contact for ${req.params.id}`});
+
+const getContact = asyncHandler(async (req, res) => {
+  res.status(200).json({ messges: `update Contact for ${req.params.id}` });
 });
-
-
 
 //description delete contact
 // routes DELETE/api/contacts/:id
 // access-> public
 // when we introduce authentication we would make it private
-const deleteContact = ((req, res)=>{
-    res.status(200).json({messges: `delete Contact for ${req.params.id}`});
+const deleteContact = asyncHandler(async (req, res) => {
+  res.status(200).json({ messges: `delete Contact for ${req.params.id}` });
 });
-
-
 
 //description update contact
 // routes PUT/api/contacts/:id
 // access-> public
 // when we introduce authentication we would make it private
-const updateContact = ((req, res)=>{
-    res.status(200).json({messges: `update Contact for ${req.params.id}`});
+const updateContact = asyncHandler(async (req, res) => {
+  res.status(200).json({ messges: `update Contact for ${req.params.id}` });
 });
 
-
-
-module.exports = {getContacts, getContact, updateContact, deleteContact, createContact};
+module.exports = {
+  getContacts,
+  getContact,
+  updateContact,
+  deleteContact,
+  createContact,
+};
